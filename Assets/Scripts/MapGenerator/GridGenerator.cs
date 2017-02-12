a11c8f08f180c021e4cad567e61a568c203a6ea6
Eduardo Nicolas Quintana <linkstudioarg@gmail.com>
2017-02-12 13:11:46 -0300

    Added MapGenerator prefab Added Node code that, for now, just informs the size of the node prefab, also added The MapGenerator, for now, just creates a blank grid, dimensions need to be setted as well as separation in the 3 axis. If u want a 2 axis grid, set the null axis to 1. Separation can be 0.

---
+++ Assets/Scripts/MapGenerator/GridGenerator.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridGenerator : MonoBehaviour {
+    public GameObject nodePrefab;
+    public Vector3 dimentions;
+  
+    public Vector3 separation;
+    private Vector3 carretPos;
+
+    public List<Node> nodes;
+    private GameObject AllNodes;
+
+    [ContextMenu("Generate Blank Grid")]
+    List<Node> GenerateBlankGrid()
+    {
+        AllNodes = new GameObject("Nodes");
+        AllNodes.transform.SetParent(this.transform);
+        nodes = new List<Node>();
+        Node newNode = nodePrefab.GetComponent<Node>();
+        GameObject newNodeGO;
+        Vector3 carretInitalPos = transform.position + newNode.GetSize() / 2;
+        carretPos = carretInitalPos;
+        for (int k = 0; k < dimentions.z; k++)
+        {
+            for (int j = 0; j < dimentions.y; j++)
+            {
+                for (int i = 0; i < dimentions.x; i++)
+                {
+                    newNodeGO = Instantiate<GameObject>(nodePrefab, carretPos, Quaternion.identity,AllNodes.transform);
+                    nodes.Add(newNodeGO.GetComponent<Node>());
+                    carretPos += transform.right * (newNode.GetSize().x + separation.x);
+                }
+                carretPos = new Vector3(carretInitalPos.x,carretPos.y , carretPos.z);
+                carretPos +=  transform.up * (newNode.GetSize().y + separation.y) ;
+            }
+            carretPos = new Vector3(carretInitalPos.x, carretInitalPos.y, carretPos.z );
+            carretPos += transform.forward * (newNode.GetSize().z + separation.z);
+        }
+        return nodes;
+    }
+
+    [ContextMenu("Clean Grid")]
+    void CleanGrid()
+    {
+        if(AllNodes != null)
+        {
+            DestroyImmediate(AllNodes);
+        }
+    }
+
+
+}
